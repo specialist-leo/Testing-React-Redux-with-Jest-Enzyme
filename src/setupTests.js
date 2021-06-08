40f59cc5e8ccb8699e69066a29b4a1a576073f9f
specialist-leo <specialist-leo@gmail.com>
2021-06-08 22:48:35 +0900

    기본 세팅

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,7 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import Enzyme from 'enzyme'; // 혹은 import { configure } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter(), // 어뎁터를 enzymeAdapter의 새 인스턴스로 생성
+  disableLifecycleMethods: true, // 수명주기 메서드 비활성화
+});
